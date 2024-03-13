8ea28c4ae97e75b3466858b1a1a7facea95f1c0a
Olha Shtepa <olgaolga2414@gmail.com>
2024-03-13 21:04:54 +0200

    create react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import CurrentDay from './components/CurrentDay/index';
+import DaysOfWeek from './components/DaysOfWeek/index';
+
+function App() {
+  return (
+    <div>
+      <CurrentDay />
+      <DaysOfWeek />
+    </div>
+  );
+}
+
+export default App;
