3d89245384aac3ebafdb4d4d2ef2e63e52d04fb3
Olha Shtepa <olgaolga2414@gmail.com>
2024-03-19 21:36:47 +0200

    get current day&number

--- src/components/CurrentDay/index.js
+++ src/components/CurrentDay/index.js
@@ -1,24 +1,30 @@
-import { format} from 'date-fns';
-import React, { Component } from 'react';
+import { format } from "date-fns";
+import React, { Component } from "react";
+import styles from "../Calendar.module.scss";
 
 class CurrentDay extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            date: new Date(),
-        }
-    }
-     
-    render() {
-        const {date} = this.state;
-        return (
-            <div>
-                <h2>{format(date,"dd" )}</h2>
-                <p>{format(date,"EEEE" )}</p>
-                <p>{format(date,"EEEE" )}</p>
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date(),
+    };
+  }
+
+  render() {
+    const { date } = this.state;
+    return (
+      <div className={styles.wrapper}>
+        <div className={styles.container}>
+          <h2>{format(date, "dd")}</h2>
+          <p>{format(date, "EEEE")}</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default CurrentDay;
+
+// const WEEK_DAYS = ["SANDAY", "M...", "T", "W", "T", "F", "S"];
+// const currentDay = getDate(currentDate);                            // поточне число
+// const currentWeekday = WEEK_DAYS[getDay(currentDate)];           // день тижня
